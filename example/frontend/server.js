14ce4c51dedc261d028d6a9f95f79c58c21dcc64
Ben Cöppicus <ben.coeppicus@bencoepp.de>
2025-10-01 14:15:41 +0200

    Add deployment and service configurations for frontend and backend, update Dockerfile to expose port 3000, and adjust server.js to use port 3000

--- example/frontend/server.js
+++ example/frontend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 // Backend URL (from env variable or default to service name in K8s)
 const BACKEND_URL = process.env.BACKEND_URL || "http://backend:3000";
